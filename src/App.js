1980f97c4af1362b856015c11b87072206227d9e
Muhammad Sabtain <subtainjutt26@gmail.com>
2024-02-21 19:59:08 +0500

    frontend ready

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
 import '../node_modules/bootstrap/dist/js/bootstrap.bundle'
 import './App.scss';
+import Home from './Home/home';
 
 function App() {
   return (
 <>
-<h1>
-  Abcd
-</h1>
+<Home />
 </>
   );
 }
---
+++ src/Home/home.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+export default function Home() {
+  return (
+    <div className="container">
+      <div className="row">
+        <div className="col mt-4 text-center">
+          <h1 style={{ fontFamily: "'Brush Script MT', cursive",fontSize:"100px" }}>Dictionary</h1>
+          <div className="image rounded-5 mt-3"></div>
+        </div>
+      </div>
+    </div>
+  );
+}
